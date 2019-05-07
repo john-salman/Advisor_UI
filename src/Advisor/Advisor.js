9652b12a47a388d9300c99bdb7e73978d12189c5
John Salman <johnfish15@gmail.com>
2019-05-07 12:11:26 -0700

    Advisor side linked up, insertion to database may be working

--- src/Advisor/Advisor.js
+++ src/Advisor/Advisor.js
@@ -9,11 +9,13 @@ import AdvisorTabs from "./AdvisorTabs";
 class Advisor extends Component{
 
         state = {
-            selectedDate: new Date()
+            selectedDate: new Date(),
+            student_data: "",
         };
 
         constructor(props) {
             super(props);
+            this.submit_add = this.submit_add.bind(this);
             this.dateSelect = this.dateSelect.bind(this);
         }
 
@@ -41,13 +43,34 @@ class Advisor extends Component{
         });
     }
 
+    submit_add(_student_fName, _student_lName, _advisingTime) {
+        console.log("This is the state:", this.state);
+        let advisee_id = -1;
+        this.state.student_data.forEach( student => {
+            if (student.student_fName === _student_fName && student.student_lName === _student_lName) {
+                advisee_id = student.student_id;
+            }
+        });
+        if (advisee_id !== -1) {
+            axios.post('meeting/postAdvisor/' + this.props.user_data.login_id + '/' + advisee_id + '/' + _advisingTime)
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        } else {
+            console.log("Error: user not found")
+        }
+    }
+
 
     render() {
     return (
         <div className="Advisor">
           <AdvisorBar fName={this.props.user_data.user_fName} lName={this.props.user_data.user_lName} logout={this.props.logout}/>
           <AdvisorTabs
-              submit_add={this.props.submit_add}
+              submit_add={this.submit_add}
               dateSelect={this.dateSelect}
               selectedDate={this.state.selectedDate}
               student_data={this.state.student_data}
